5b16a95fd6de338a8260343ccbcc1f69dd567f21
Darkmeedo <113542244+Darkmeedo@users.noreply.github.com>
2024-11-18 16:04:18 +0100

    Update miniscripts.js

--- assets/miniscripts.js
+++ assets/miniscripts.js
@@ -1,1 +1,16 @@
 $(document).ready(function(){$('.gallery').mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:'myAwesomeLightbox',showTags:!0,tagsPosition:'top'})})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector('#carouselExampleIndicators');
+    const carouselImages = carousel.querySelectorAll('.carousel-item img');
+
+    carousel.addEventListener('slide.bs.carousel', function(event) {
+        const nextSlide = event.relatedTarget;
+        const img = nextSlide.querySelector('img');
+        
+        if (img.dataset.src) {
+            img.src = img.dataset.src;
+            img.removeAttribute('data-src');
+        }
+    });
+});
